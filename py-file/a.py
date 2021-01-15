730e9dbf6838cc4ef9187985e16354ac18150235
jxxxxx <44454049@qq.com>
2021-01-15 17:48:49 +0800

    新文件：   jd-info

--- py-file/a.py
+++ py-file/a.py
@@ -1,8 +1,13 @@
 #/usr/bin/python
+def add(a, b):
+    return a+b
+#equals to print add(4, 3)
 
-var1='hellp!world!'
-var2="python here"
+def one(a,*b):
+    print(b),type(b)
+one(1,2,3,4,5,6)
 
-print "var1[0]:",var1[0]
-print "var1[0:1]",var1[0:2]
-print var1[:-1]*2
+def two(a=1,**b):
+    print(b),type(b)
+two(x1=1,b=2,c=3,d=4,e=5,f=6)
+
