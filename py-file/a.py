077de591e90e88a0292f1fdea3245c88a8b7a627
jxxxxx <44454049@qq.com>
2021-01-25 10:45:38 +0800

    修改：     org-file/todo.org

--- py-file/a.py
+++ py-file/a.py
@@ -1,13 +1,10 @@
 #/usr/bin/python
-def add(a, b):
-    return a+b
-#equals to print add(4, 3)
+seq = ('Google', 'Runoob', 'Taobao')
 
-def one(a,*b):
-    print(b),type(b)
-one(1,2,3,4,5,6)
 
-def two(a=1,**b):
-    print(b),type(b)
-two(x1=1,b=2,c=3,d=4,e=5,f=6)
+
+
+
+
+
 
